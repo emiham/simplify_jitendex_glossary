3e9230848ae23c87e948d716eebe34698bc065cd
Emil Hammarberg <emilhammarberg@mailbox.org>
2025-04-09 14:08:43 +0200

    Properly count edited notes

--- simplify_jitendex_glossary.py
+++ simplify_jitendex_glossary.py
@@ -14,17 +14,12 @@ def setup_menu(browser):
     action.triggered.connect(lambda _, b=browser: simplify_notes(b))
 
 
-def simplify_note(note):
-    glossary_field = mw.addonManager.getConfig(__name__)["glossary_field"]
-    if glossary_field not in note.keys():
-        return note
-
+def simplify_field_content(field):
     sense_separator = mw.addonManager.getConfig(__name__)["sense_separator"]
     subsense_separator = mw.addonManager.getConfig(__name__)["subsense_separator"]
 
-    soup = BeautifulSoup(note[glossary_field], "html.parser")
+    soup = BeautifulSoup(field, "html.parser")
     definitions = []
-
 
     content = soup.findAll(attrs={"data-sc-content": "glossary"})
     if content:
@@ -35,19 +30,25 @@ def simplify_note(note):
             if explanation:
                 explanation = explanation.get_text(strip=True, separator=" ")
                 explanation = explanation.replace("Explanation", "", 1).strip()
-                definitions.append(subsense_separator.join(glosses) + f" ({explanation})")
+                definitions.append(
+                    subsense_separator.join(glosses) + f" ({explanation})"
+                )
 
             else:
                 definitions.append(subsense_separator.join(glosses))
 
-        note[glossary_field] = sense_separator.join(definitions)
+        return sense_separator.join(definitions)
+
+
+def simplify_note(note):
+    glossary_field = mw.addonManager.getConfig(__name__)["glossary_field"]
+    if glossary_field not in note.keys():
+        return note
+
+    simplified_field_content = simplify_field_content(note[glossary_field])
+    if simplified_field_content and simplified_field_content != note[glossary_field]:
+        note[glossary_field] = simplify_field_content(note[glossary_field])
     return note
-
-
-def on_flush_note(note):
-    if mw.addonManager.getConfig(__name__)["modify_new"]:
-        simplify_note(note)
-    return
 
 
 def simplify_notes(browser):
@@ -59,14 +60,29 @@ def simplify_notes(browser):
     notes = []
     undo = mw.col.add_custom_undo_entry(NAME)
     mw.progress.start()
+    glossary_field = mw.addonManager.getConfig(__name__)["glossary_field"]
+
     for note_id in note_ids:
-        notes.append(simplify_note(mw.col.get_note(note_id)))
+        note = mw.col.get_note(note_id)
+        if glossary_field in note.keys():
+            simplified_field_content = simplify_field_content(note[glossary_field])
+            if (
+                simplified_field_content
+                and simplified_field_content != note[glossary_field]
+            ):
+                notes.append(simplify_note(note))
 
     mw.col.update_notes(notes)
     mw.col.merge_undo_entries(undo)
     tooltip(tr.browsing_notes_updated(len(notes)), period=2000)
     mw.progress.finish()
     mw.reset()
+
+
+def on_flush_note(note):
+    if mw.addonManager.getConfig(__name__)["modify_new"]:
+        simplify_note(note)
+    return
 
 
 gui_hooks.browser_menus_did_init.append(setup_menu)
